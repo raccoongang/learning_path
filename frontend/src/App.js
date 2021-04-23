b5d11d2377339af2d6b0adbe66789102121db1f3
Vladas Tamoshaitis <vladas.tamoshaitis@raccoongang.com>
2021-04-23 18:58:59 +0300

    feat: Render node structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import {PathwayNode} from './Node';
+import * as data from './mock.json';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const nodeData = data.default;
+    return (
+        <div className="container">
+           <PathwayNode {...nodeData} />
+        </div>
+    );
 }
 
 export default App;
