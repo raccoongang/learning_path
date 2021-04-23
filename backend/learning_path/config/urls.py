278d6d17a60b8c598266d4ffa581b19f858635a1
Vladas Tamoshaitis <amd.vladas@gmail.com>
2021-04-23 20:25:49 +0300

    Add auth (#2)
    * feat: Render node structure
    
    * feat: add node component
    
    * feat: add google auth
    
    * feat: git ignore secrets
    
    * fix: compose volume
    
    Co-authored-by: Vladas Tamoshaitis <vladas.tamoshaitis@raccoongang.com>

--- backend/learning_path/config/urls.py
+++ backend/learning_path/config/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth.views import LogoutView
+from django.views.generic import TemplateView
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name="auth/index.html")),
+    path('accounts/', include('allauth.urls')),
+    path('logout', LogoutView.as_view()),
 ]
